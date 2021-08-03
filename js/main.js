5812c2f5869c8baaa4b253dee3d3d24bdb675a5d
Juan Hernández <hajjose7@gmail.com>
2021-08-02 21:39:06 -0400

    Finalizado servicios y portafolio

--- js/main.js
+++ js/main.js
@@ -31,13 +31,13 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // ======= Acordion skills ================
 const skillsContent = document.getElementsByClassName('skills__content'),
-    skillsHeader = document.querySelectorAll('.skills__header')
+    skillsHeader = document.querySelectorAll('.skills__header');
 
 function toggleSkills() {
-    let itemClass = this.parentNode.className
+    let itemClass = this.parentNode.className;
 
     for(i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills__content skills__close'
+        skillsContent[i].className = 'skills__content skills__close';
     }
     if(itemClass === 'skills__content skills__close') {
         this.parentNode.className = 'skills__content skills__open';
@@ -47,3 +47,59 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
+
+/* ======== QUALIFICATION TABS ======== */
+const tabs = document.querySelectorAll('[data-target]'),
+    tabsContents = document.querySelectorAll('[data-content]')
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabsContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        });
+        tab.classList.add('qualification__active');
+    });
+});
+
+// ========= SERVICES MODAL ===========
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    });
+});
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        });
+    });
+});
+
+// ============== SWIPER PORTFOLIO ================
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
